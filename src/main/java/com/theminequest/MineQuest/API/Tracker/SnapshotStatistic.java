c1d48b7485aea17957e72b4e672343b141c11052
Robert Xu <robxu9@gmail.com>
2012-07-02 10:43:13 -0400

    LogStatistic, SnapshotStatistic, and LogStatus
    Signed-off-by: Robert Xu <robxu9@gmail.com>

---
+++ src/main/java/com/theminequest/MineQuest/API/Tracker/SnapshotStatistic.java
@@ -0,0 +1,52 @@
+package com.theminequest.MineQuest.API.Tracker;
+
+import com.alta189.simplesave.Field;
+import com.alta189.simplesave.Id;
+import com.alta189.simplesave.Table;
+import com.theminequest.MineQuest.API.Quest.QuestSnapshot;
+import com.theminequest.MineQuest.API.Tracker.StatisticManager.Statistic;
+
+@Table("mq_snapshots")
+public class SnapshotStatistic extends Statistic {
+	
+	@Id
+	private long snapshotid;
+	
+	@Field
+	private String questName;
+	
+	@Field
+	private QuestSnapshot snapshot;
+	
+	public String getQuestName() {
+		return questName;
+	}
+
+	public void setQuestName(String questName) {
+		this.questName = questName;
+	}
+
+	public QuestSnapshot getSnapshot() {
+		return snapshot;
+	}
+
+	public void setSnapshot(QuestSnapshot snapshot) {
+		this.snapshot = snapshot;
+	}
+
+	@Override
+	public void setup() {}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof String) {
+			return questName.equals(obj);
+		} else if (obj instanceof SnapshotStatistic) {
+			SnapshotStatistic s = (SnapshotStatistic) obj;
+			if (questName.equals(s.questName) && snapshot.equals(s.snapshot))
+				return true;
+		}
+		return false;
+	}
+
+}
